3392290b1dff682b7d30783382c856e6dadf2a07
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-02-16 23:36:24 +0300

    Add validation rules

---
+++ CustomValidation/ValidationRules/ComparableValidationRules.cs
@@ -0,0 +1,25 @@
+﻿namespace CustomValidation.ValidationRules
+{
+    public static class ComparableValidationRules<T> where T : IComparable<T>
+    {
+        public static bool AddLesThanValidationRule(T property, T valueToCompare)
+        {
+            if (property.CompareTo(valueToCompare) >= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool AddGreaterThanValidationRule(T property, T valueToCompare)
+        {
+            if (property.CompareTo(valueToCompare) <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CustomValidation/ValidationRules/StringValidationRules.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CustomValidation.ValidationRules
+{
+    public static class StringValidationRules
+    {
+        public static bool AddMinLengthValidationRule(string property, string condition)
+        {
+            var minLength = Convert.ToInt32(condition);
+
+            if (property.Length < minLength)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool AddMaxLengthValidationRule(string property, string condition)
+        {
+            var maxLength = Convert.ToInt32(condition);
+
+            if (property.Length > maxLength)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool AddRegexValidationRule(string property, string pattern)
+        {
+            Regex regex = new Regex(pattern);
+
+            if (!regex.IsMatch(property))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool AddEqualValidationRule(string property, string condition)
+        {
+            if (property != condition)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool AddNotEqualValidationRule(string property, string condition)
+        {
+            if (property == condition)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
