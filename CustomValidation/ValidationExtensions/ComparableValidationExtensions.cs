fa754756b9133285dd93c8c6ea8e2a12ab014128
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-02-16 23:37:30 +0300

    Add validation extensions

---
+++ CustomValidation/ValidationExtensions/ComparableValidationExtensions.cs
@@ -0,0 +1,139 @@
+﻿using CustomValidation.ValidationRules;
+
+namespace CustomValidation.ValidationExtensions
+{
+    public static class ComparableValidationExtensions
+    {
+        public static ValidationChain<int> AddLesThanValidation(this ValidationChain<int> validationChain,
+            int valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<int>.AddLesThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<double> AddLesThanValidation(this ValidationChain<double> validationChain,
+            double valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<double>.AddLesThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<float> AddLesThanValidation(this ValidationChain<float> validationChain,
+            float valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<float>.AddLesThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<DateTime> AddLesThanValidation(this ValidationChain<DateTime> validationChain,
+            DateTime valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<DateTime>.AddLesThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<DateOnly> AddLesThanValidation(this ValidationChain<DateOnly> validationChain,
+            DateOnly valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<DateOnly>.AddLesThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<TimeOnly> AddLesThanValidation(this ValidationChain<TimeOnly> validationChain,
+            TimeOnly valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<TimeOnly>.AddLesThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<int> AddGreaterThanValidation(this ValidationChain<int> validationChain,
+            int valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<int>.AddGreaterThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<double> AddGreaterThanValidation(this ValidationChain<double> validationChain,
+            double valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<double>.AddGreaterThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<float> AddGreaterThanValidation(this ValidationChain<float> validationChain,
+            float valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<float>.AddGreaterThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<DateTime> AddGreaterThanValidation(this ValidationChain<DateTime> validationChain,
+            DateTime valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<DateTime>.AddGreaterThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<DateOnly> AddGreaterThanValidation(this ValidationChain<DateOnly> validationChain,
+            DateOnly valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<DateOnly>.AddGreaterThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<TimeOnly> AddGreaterThanValidation(this ValidationChain<TimeOnly> validationChain,
+            TimeOnly valueToCompare)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            ComparableValidationRules<TimeOnly>.AddGreaterThanValidationRule(property, condition));
+
+            validationChain.Conditions.Add(valueToCompare);
+
+            return validationChain;
+        }
+    }
+}
---
+++ CustomValidation/ValidationExtensions/StringValidationExtensions.cs
@@ -0,0 +1,86 @@
+﻿using CustomValidation.ValidationRules;
+
+namespace CustomValidation.ValidationExtensions
+{
+    public static class StringValidationExtensions
+    {
+        private const string EmailPattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+            + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+            + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+        private const string PhonePattern = @"^\+(?:[0-9]●?){6,14}[0-9]$";
+
+        public static ValidationChain<string> AddRegexValidation(this ValidationChain<string> validationChain,
+            string pattern)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            StringValidationRules.AddRegexValidationRule(property, condition));
+
+            validationChain.Conditions.Add(pattern);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<string> AddEmailValidation(this ValidationChain<string> validationChain)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            StringValidationRules.AddRegexValidationRule(property, condition));
+
+            validationChain.Conditions.Add(EmailPattern);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<string> AddPhoneValidation(this ValidationChain<string> validationChain)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            StringValidationRules.AddRegexValidationRule(property, condition));
+
+            validationChain.Conditions.Add(PhonePattern);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<string> AddMaxLengthValidation(this ValidationChain<string> validationChain,
+            int maxLength)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            StringValidationRules.AddMaxLengthValidationRule(property, condition));
+
+            validationChain.Conditions.Add(Convert.ToString(maxLength));
+
+            return validationChain;
+        }
+
+        public static ValidationChain<string> AddMinLengthValidation(this ValidationChain<string> validationChain,
+            int minLength)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            StringValidationRules.AddMinLengthValidationRule(property, condition));
+
+            validationChain.Conditions.Add(Convert.ToString(minLength));
+
+            return validationChain;
+        }
+
+        public static ValidationChain<string> AddNotEmptyValidation(this ValidationChain<string> validationChain)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            StringValidationRules.AddNotEqualValidationRule(property, condition));
+
+            validationChain.Conditions.Add(string.Empty);
+
+            return validationChain;
+        }
+
+        public static ValidationChain<string> AddNotNullValidation(this ValidationChain<string> validationChain)
+        {
+            validationChain.Rules.Add((property, condition) =>
+            StringValidationRules.AddNotEqualValidationRule(property, condition));
+
+            validationChain.Conditions.Add(null);
+
+            return validationChain;
+        }
+    }
+}
